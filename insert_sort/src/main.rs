310da01efdf1bf70f0f6cd4e8229d33c335b359c
stevemac321 <stevemac321@live.com>
2024-08-26 08:02:11 -0700

    added generic sort

--- insert_sort/src/main.rs
+++ insert_sort/src/main.rs
@@ -1,3 +1,53 @@
+
+fn test_gen_ins() {
+    {
+
+    println!("gen sort less");
+    let less = |x,y| x < y;
+    let mut test1 = vec![5, 2, 9, 1, 5, 6];
+    let mut test2 = vec![3, 0, -1, 8, 7, 2];
+    let mut test3 = vec![10, 20, 30, 40, 50];
+    let mut test4 = vec![50, 40, 30, 20, 10];
+    let mut test5 = vec![1, 2, 3, 4, 5];
+
+    gen_ins_sort(&mut test1, less);
+    gen_ins_sort(&mut test2, less);
+    gen_ins_sort(&mut test3, less);
+    gen_ins_sort(&mut test4, less);
+    gen_ins_sort(&mut test5, less);
+
+    println!("Sorted test1: {:?}", test1);
+    println!("Sorted test2: {:?}", test2);
+    println!("Sorted test3: {:?}", test3);
+    println!("Sorted test4: {:?}", test4);
+    println!("Sorted test5: {:?}", test5);
+    }
+    {
+    println!("gen sort greater");
+    let greater = |x,y| x > y;
+    let mut test1 = vec![5, 2, 9, 1, 5, 6];
+    let mut test2 = vec![3, 0, -1, 8, 7, 2];
+    let mut test3 = vec![10, 20, 30, 40, 50];
+    let mut test4 = vec![50, 40, 30, 20, 10];
+    let mut test5 = vec![1, 2, 3, 4, 5];
+
+    gen_ins_sort(&mut test1, greater);
+    gen_ins_sort(&mut test2, greater);
+    gen_ins_sort(&mut test3, greater);
+    gen_ins_sort(&mut test4, greater);
+    gen_ins_sort(&mut test5, greater);
+
+    println!("Sorted test1: {:?}", test1);
+    println!("Sorted test2: {:?}", test2);
+    println!("Sorted test3: {:?}", test3);
+    println!("Sorted test4: {:?}", test4);
+    println!("Sorted test5: {:?}", test5);
+    }
+}
+
+
+
+
 fn test_ins() {
     let mut test1 = vec![5, 2, 9, 1, 5, 6];
     let mut test2 = vec![3, 0, -1, 8, 7, 2];
@@ -67,4 +117,22 @@ fn sel_sort(arr: &mut [i32]) {
 fn main() {
     test_sel();
     test_ins();
+    test_gen_ins();
 }
+
+fn gen_ins_sort<Compare>(arr: &mut [i32], cmp: Compare)
+where
+    Compare: Fn(i32, i32) ->bool,
+{
+    for j in 1..arr.len() {
+        let key = arr[j];    
+        let mut i = j;      
+
+        while i > 0 && cmp(key, arr[i - 1]) { // swap args so that calling with "less" implies
+            arr[i] = arr[i - 1];              // ascending sorted order 
+            i -= 1;                        
+        }
+        arr[i] = key;                     
+    }
+}
+
